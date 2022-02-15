e8955dce5d1f28fb10a70471b8ac2fbf58b3eb7e
AnastasiiaNehriienko <nastianegrienko@gmail.com>
2022-02-15 10:27:36 +0200

    Добавьте файлы проекта.

---
+++ Lab1_C/Header.h
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+void give_names(string& n1, string& n2) {
+	cout << "Enter the name of file: ";
+	cin >> n1;
+	n1 += ".txt";
+	cout << "Enter the name of new file: ";
+	cin >> n2;
+	n2 += ".txt";
+}
+void create_txt(string n) {
+	string txt;
+	ofstream outFile(n);
+	cout << "Enter your text here: ";
+	do {                                //зчитування тексту у файл до закінчення (Ctrl+z)
+		getline(cin, txt);
+		outFile << txt << endl;
+	} while (cin);
+	outFile.close();
+}
+void print_txt(string n) {
+	ifstream inF(n);
+	string s;
+	do {                                //зчитування тексту з файлу
+		getline(inF, s);
+		cout << s << endl;
+	} while (inF);
+	inF.close();
+}
+void changing(string n, string m) {
+	ifstream inF(n);
+	ofstream outF(m);
+	string s;
+	int empty;          //місце останнього пробілу
+	int if_space_end;   //якщо в кінці рядка пробіл
+	char letter;        //літера, що вставлятиметься на початок
+	do {
+		getline(inF, s);
+		empty = s.rfind(" ");  //пошук останнього пробілу
+		if (empty == string::npos)  //умова: єдине слово
+			letter = s[0];
+		else if (empty == s.length() - 1) {          //умова: пробіл в кінці
+			if_space_end = s.find_last_not_of(" ");  //остання літера
+			s.erase(if_space_end + 1);               //видалення пробілів в кінці 
+			empty = s.rfind(" ");
+			letter = s[empty + 1];
+		}
+		else
+			letter = s[empty + 1];
+		outF << letter << s << endl;
+	} while (inF);
+	inF.close();
+	outF.close();
+}
---
+++ Lab1_C/Lab1_C.cpp
@@ -0,0 +1,16 @@
+﻿#include "Header.h"
+void give_names(string&, string&); //надання імен файлам
+void create_txt(string);           //створення нового файлуб введення тексту
+void print_txt(string);            //друкування файлу
+void changing(string, string);     //створення нового файлу зі зміненим текстом
+int main() {
+	string name, name2;           //імена файлів
+	give_names(name, name2);
+	create_txt(name);
+	cout << "Your text is: ";
+	print_txt(name);
+	changing(name, name2);
+	cout << "New text is: ";
+	print_txt(name2);
+	return 0;
+}
